d97d8b4026ffba78e0fc5430b4c6a48c35a7588b
GUGOFO <gugofogomes@gmail.com>
2026-09-05 15:17:52 -0300

    Solver A*

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,33 @@
-import { gerarGrade, renderizarVeiculos } from './tabuleiro.js';
+import { gerarGrade, renderizarVeiculos, veiculos, celulaOcupada } from './tabuleiro.js';
 import { selecionarVeiculo, inicializarControles } from './controles.js';
+import { SolverAStar } from './solverAStar.js';
+
+export function resolverAutomaticamente() {
+  const solver = new SolverAStar(veiculos);
+  const solucao = solver.resolver();
+
+  if (!solucao) {
+    alert("Esta fase não possui solução!");
+    return;
+  }
+
+  console.log(`Mínimo de movimentos necessários: ${solucao.length}`);
+
+  solucao.forEach((acao, index) => {
+    setTimeout(() => {
+      const veiculo = veiculos.find(v => v.id === acao.id);
+      if (veiculo) {
+        veiculo.mover(acao.passo, celulaOcupada);
+      }
+    }, index * 350); 
+  });
+}
 
 gerarGrade();
 renderizarVeiculos(selecionarVeiculo);
 inicializarControles();
+
+const btnResolver = document.getElementById("btn-resolver");
+if (btnResolver) {
+  btnResolver.addEventListener("click", resolverAutomaticamente);
+}
